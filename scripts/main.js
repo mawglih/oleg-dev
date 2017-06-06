1c0995e9aee14148caf5678ae089aa08be363f00
Oleg Markoff <mawglih@gmail.com>
2017-06-06 16:50:08 -0400

    second commit

--- scripts/main.js
+++ scripts/main.js
@@ -4,3 +4,12 @@ $("#navbarToggle").blur(function(event) {
         $("#js-navbar-collapse").collapse('hide');
     }
 });
+$('#btn-main-hover').mouseenter(function(){
+    $('.main-primary.bg-image > img').css('opacity', 0.75);
+    $('#btn-main-hover').css('font-weight', 'bold');
+    $('#btn-main-hover').css('color', 'white');
+});
+$('#btn-main-hover').mouseleave(function(){
+    $('.main-primary.bg-image > img').css('opacity', 0.55);
+    $('#btn-main-hover').css('font-weight', 'normal');
+});
