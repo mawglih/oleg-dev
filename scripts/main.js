c4ec2dfcbf7e16e002a136257bbc16ae5cda9e63
Oleg Markoff <mawglih@gmail.com>
2017-06-07 16:42:27 -0400

    June7 first commit

--- scripts/main.js
+++ scripts/main.js
@@ -13,3 +13,19 @@ $('#btn-main-hover').mouseleave(function(){
     $('.main-primary.bg-image > img').css('opacity', 0.55);
     $('#btn-main-hover').css('font-weight', 'normal');
 });
+$('.main-secondary').mouseenter(function(){
+    $('.main-secondary.bg-image > img').css('opacity', 0.75);
+    $('.main-secondary').css('font-weight', 'bold');
+    $('#body-sec>i').css('margin-right', '15px');
+});
+$('.main-secondary').mouseleave(function(){
+    $('.main-secondary.bg-image > img').css('opacity', 0.55);
+    $('.main-secondary').css('font-weight', 'normal');
+    $('#body-sec>i').css('margin-right', '10px');
+});
+$('#footer-span2').mouseenter(function(){
+    $('#check-hidden').css('visibility', 'visible');
+});
+$('#footer-span2').mouseleave(function(){
+    $('#check-hidden').css('visibility', 'hidden');
+});
