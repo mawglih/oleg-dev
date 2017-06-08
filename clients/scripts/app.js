d9735396f3c0707a1e0c8838ae31cd03b7279983
Oleg Markoff <mawglih@gmail.com>
2017-06-08 14:30:56 -0400

    angular started

--- clients/scripts/app.js
+++ clients/scripts/app.js
@@ -1,4 +1,24 @@
-'use strict';
+/**
+ * @ngdoc overview
+ * @name clApp
+ * @description
+ * # clApp
+ *
+ * Main module of the application.
+ */
 (function(){
+'use strict';
+    angular
+    .module('clApp',['ui.router'])
+    .config(RoutesConfig);
 
+    RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+    function RoutesConfig($stateProvider, $urlRouterProvider){
+        $urlRouterProvider.otherwise('/');
+        $stateProvider
+        .state('main', {
+            url: '/',
+            templateUrl:'views/main.html'
+        });
+    }
 })();
---
+++ clients/scripts/components/footer.js
@@ -0,0 +1,17 @@
+/**
+ * @ngdoc overview
+ * @name clApp
+ * @description
+ * # clApp
+ *
+ * Main module of the application.
+ */
+(function(){
+'use strict';
+    angular.module('clApp')
+    .component('footer',{
+        templateUrl:'views/templates/footer.html'
+    });
+
+
+})();
